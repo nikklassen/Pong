a765b08e635e83dd096f9f2a33f6e53ba9cd46c0
Nik Klassen <Nik@Niks-MacBook-Pro.local>
2012-12-30 17:02:28 -0500

    Initial Commit

---
+++ Pong/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Pong
+//
+//  Created by Nik Klassen on 2012-12-30.
+//  Copyright (c) 2012 Nik Klassen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
